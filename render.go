2ee90e8c8d97c7ef9d4a190b9aef3ac5d5e4ab39
David Millman <dave@cs.unc.edu>
2025-04-28 20:18:36 -0600

    Add render tests

--- render.go
+++ render.go
@@ -1,96 +1,65 @@
 package pluck
 
 import (
+	"fmt"
 	"io"
-	"strings"
-	"unicode"
 )
 
-func removeFirst(code string, toRemove string) string {
-	return strings.Replace(code, string(toRemove), "", 1)
-}
+type RenderFunc func(io.Writer, *DB) error
 
-func removeLast(code string, toRemove string) string {
-	idx := strings.LastIndex(code, string(toRemove))
-	// -1 means that the string was not found
-	if idx == -1 {
-		return code
-	}
-	return code[:idx] + code[idx+len(toRemove):]
-}
-
-func breakIntoLines(code string) []string {
-	return strings.Split(code, "\n")
-}
-
-func trimLeadingBlankLines(lines []string) []string {
-	firstNonBlankLine := 0
-	for ; firstNonBlankLine < len(lines); firstNonBlankLine++ {
-		line := lines[firstNonBlankLine]
-		if strings.TrimSpace(line) != "" {
-			break
+func write(w io.Writer, hunks ...string) error {
+	for _, hunk := range hunks {
+		if _, err := w.Write([]byte(hunk)); err != nil {
+			return fmt.Errorf("writing hunk: %w", err)
 		}
 	}
-
-	return lines[firstNonBlankLine:]
+	return nil
 }
 
-func trimTrailingBlankLines(lines []string) []string {
-	lastNonBlankLine := len(lines) - 1
-	for ; 0 <= lastNonBlankLine; lastNonBlankLine-- {
-		line := lines[lastNonBlankLine]
-		if strings.TrimSpace(line) != "" {
-			break
+func RenderBlankLine() RenderFunc {
+	return func(w io.Writer, _ *DB) error {
+		_, err := w.Write([]byte("\n"))
+		return err
+	}
+}
+
+func RenderFunction(name string) RenderFunc {
+	return func(w io.Writer, db *DB) error {
+		f, err := db.FindFunctionByName(name)
+		if err != nil {
+			return fmt.Errorf("querying db for function '%s': %w", name, err)
+		}
+
+		err = write(w, f.DocString, "\n", f.Definition, "\n")
+		if err != nil {
+			return fmt.Errorf("writing function '%s': %w", name, err)
+		}
+
+		return err
+	}
+}
+
+func RenderType(name string) RenderFunc {
+	return func(w io.Writer, db *DB) error {
+		t, err := db.FindTypeByName(name)
+		if err != nil {
+			return fmt.Errorf("querying db for type '%s': %w", name, err)
+		}
+
+		err = write(w, t.DocString, "\n", t.Definition, "\n")
+		if err != nil {
+			return fmt.Errorf("writing type '%s': %w", name, err)
+		}
+
+		return nil
+	}
+}
+
+func Render(w io.Writer, db *DB, rf ...RenderFunc) error {
+	for _, f := range rf {
+		if err := f(w, db); err != nil {
+			return fmt.Errorf("applying render func: %w", err)
 		}
 	}
-
-	return lines[:lastNonBlankLine+1]
-}
-
-func getLinePefix(lines []string) string {
-	if len(lines) == 0 {
-		return ""
-	}
-
-	s := lines[0]
-	var b strings.Builder
-	for _, r := range s {
-		if !unicode.IsSpace(r) {
-			break
-		}
-		b.WriteRune(r)
-	}
-	return b.String()
-}
-
-func trimPrefix(lines []string, prefix string) []string {
-	trimmedLines := make([]string, len(lines))
-	for i, line := range lines {
-		trimmedLines[i] = removeFirst(line, prefix)
-	}
-	return trimmedLines
-}
-
-func splitToLines(code string) []string {
-	return strings.Split(code, "\n")
-}
-
-func joinToCode(lines []string) string {
-	return strings.Join(lines, "\n") + "\n"
-}
-
-func Render(w io.Writer, fn *Function) error {
-	code := removeFirst(fn.Body, "{")
-	code = removeLast(code, "}")
-	lines := splitToLines(code)
-	lines = trimLeadingBlankLines(lines)
-	lines = trimTrailingBlankLines(lines)
-
-	prefix := getLinePefix(lines)
-	lines = trimPrefix(lines, prefix)
-
-	rendered := joinToCode(lines)
-
-	w.Write([]byte(rendered))
 	return nil
 }
---
+++ render_test.go
@@ -0,0 +1,92 @@
+package pluck_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"go.bky.sh/pluck"
+)
+
+func TestRenderBlankLine(t *testing.T) {
+	t.Run("happy path", func(t *testing.T) {
+		// given
+		buf := &bytes.Buffer{}
+		f := pluck.RenderBlankLine()
+
+		// when
+		err := f(buf, nil)
+		require.NoError(t, err)
+
+		// then
+		assert.Equal(t, "\n", buf.String())
+	})
+}
+
+func TestRenderFunction(t *testing.T) {
+	db := &pluck.DB{
+		Functions: []*pluck.Function{
+			{Name: "foo", Definition: "fooDef", DocString: "fooDoc"},
+		},
+	}
+
+	t.Run("happy path", func(t *testing.T) {
+		// given
+		buf := &bytes.Buffer{}
+		f := pluck.RenderFunction("foo")
+
+		// when
+		err := f(buf, db)
+		require.NoError(t, err)
+
+		// then
+		assert.Equal(t, "fooDoc\nfooDef\n", buf.String())
+	})
+
+	t.Run("not in db", func(t *testing.T) {
+		// given
+		buf := &bytes.Buffer{}
+		f := pluck.RenderFunction("bar")
+
+		// when
+		err := f(buf, db)
+
+		// then
+		assert.ErrorIs(t, err, pluck.ErrNotFound)
+	})
+}
+
+func TestRenderType(t *testing.T) {
+	db := &pluck.DB{
+		Types: []*pluck.Type{
+			{Name: "foo", Definition: "fooDef", DocString: "fooDoc"},
+		},
+	}
+
+	t.Run("happy path", func(t *testing.T) {
+		// given
+		buf := &bytes.Buffer{}
+		f := pluck.RenderType("foo")
+
+		// when
+		err := f(buf, db)
+		require.NoError(t, err)
+
+		// then
+		assert.Equal(t, "fooDoc\nfooDef\n", buf.String())
+	})
+
+	t.Run("not in db", func(t *testing.T) {
+		// given
+		buf := &bytes.Buffer{}
+		f := pluck.RenderFunction("bar")
+
+		// when
+		err := f(buf, db)
+
+		// then
+		assert.ErrorIs(t, err, pluck.ErrNotFound)
+	})
+}
