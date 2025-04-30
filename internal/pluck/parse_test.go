aa0844c6c3cca18c17ccaf7517c8d19bb78b15ae
David Millman <dave@cs.unc.edu>
2025-04-30 15:12:27 -0600

    Update imports

--- internal/pluck/parse_test.go
+++ internal/pluck/parse_test.go
@@ -5,22 +5,22 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"go.bky.sh/pluck"
+	"go.bky.sh/pluck/internal/pluck"
 )
 
 func TestParsePick(t *testing.T) {
 	t.Run("invalid format", func(t *testing.T) {
-		_, err := pluck.ParsePick("invalid-format")
+		_, err := pluck.ParsePickCmd("invalid-format")
 		assert.ErrorContains(t, err, "invalid format")
 	})
 
 	t.Run("empty name", func(t *testing.T) {
-		_, err := pluck.ParsePick("function:")
+		_, err := pluck.ParsePickCmd("function:")
 		assert.ErrorContains(t, err, "empty name")
 	})
 
 	t.Run("invalid kind", func(t *testing.T) {
-		_, err := pluck.ParsePick("foo:bar")
+		_, err := pluck.ParsePickCmd("foo:bar")
 		assert.ErrorContains(t, err, "invalid kind")
 	})
 }
